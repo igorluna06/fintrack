9b54e286cfd09a539fdd3f984f1afcd443fdbf8d
igorluna06 <igorluna2006@gmail.com>
2026-06-30 18:57:49 -0300

    Initial setup e dependencies

---
+++ src/main/java/com/igordev/sistema_financeiro/SistemaFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package com.igordev.sistema_financeiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaFinanceiroApplication.class, args);
+	}
+
+}
